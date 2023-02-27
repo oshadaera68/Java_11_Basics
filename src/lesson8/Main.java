36860f80db95fcb3683d1a9538f654944f27d535
oshadaera68 <mindartlk010@gmail.com>
2023-02-27 15:40:15 +0530

    added varible declaratiosn

---
+++ src/lesson8/Main.java
@@ -0,0 +1,10 @@
+package lesson8;
+
+public class Main {
+    public static void main(String[] args) {
+        int zero = 0;
+        int one = 1;
+        double pi = 2.1415;
+        int subscriberCount = 1_000_000;
+    }
+}
