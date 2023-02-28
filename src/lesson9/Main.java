96872705fd27ca58da987d4882d68113cdd167d1
oshadaera68 <mindartlk010@gmail.com>
2023-02-28 10:23:19 +0530

    string class finished

---
+++ src/lesson9/Main.java
@@ -0,0 +1,14 @@
+package lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+        //String
+        String name = "Oshada";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(1));
+        String code = "shs";
+        System.out.println(name.contains(code));
+        System.out.println(name.equals(code));
+    }
+}
